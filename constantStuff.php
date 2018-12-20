68c14c2ed1ab8b349d6f9423c78e2fac2f89e1bd
PSSEA\bkielpinski <bryan.kielpinski52@gmail.com>
2018-12-20 14:35:21 -0700

    adding a couple of examples to constant file

--- constantStuff.php
+++ constantStuff.php
@@ -2,4 +2,18 @@
     define("MINSIZE", 50);
     echo MINSIZE;
     echo constant("MINSIZE");
+
+    // Valid constant names
+    define("ONE",     "first thing");
+    define("TWO2",    "second thing");
+    define("THREE_3", "third thing");
+
+    // Invalid constant names
+    define("2TWO",    "second thing");
+    define("__THREE__", "third value"); 
+
+    echo constant("ONE");
+    echo constant("TWO2");
+    echo constant("THREE_3");
+
 ?>
